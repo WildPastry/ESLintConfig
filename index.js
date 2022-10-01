69af4a4cb64c1be0a5a967b648acd1541c883d7c
WildPastry <mykdsn@gmail.com>
2022-10-02 10:45:50 +1300

    Update version

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-module.exports = {
-
-  globals: {
-      MyGlobal: true
-  },
-
-  rules: {
-      semi: [2, "always"]
-  }
-
+export const globals = {
+  MyGlobal: true
 };
 
-exports.printMsg = function() {
+export const rules = {
+  semi: [2, "always"]
+};
+
+export function printMsg() {
   console.log("This is a message from the demo package");
 }
